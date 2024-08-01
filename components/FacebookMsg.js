a6013d47beeae200d8431e0eb59734e6f82df05c
unknown <ngohanam2709@gmail.com>
2024-08-01 18:44:03 +0700

    a

---
+++ components/FacebookMsg.js
@@ -0,0 +1,12 @@
+import React, { Component} from 'react';
+import { FacebookProvider, CustomChat } from 'react-facebook';
+
+const FacebookMsg = () => {
+    return (
+      <FacebookProvider appId="1521376062084598" chatSupport>
+        <CustomChat pageId="61563111945483" minimized={false}/>
+      </FacebookProvider>    
+    );
+}
+
+export default FacebookMsg
--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import VideoBG from '../../components/VideoBG';
 import Menu from '../../components/Menu';
 import TailorDetails from '../../components/TailorDetails';
+import FacebookMsg from '../../components/FacebookMsg'
 
 const Page = () => {
   return (
@@ -11,6 +12,7 @@ const Page = () => {
       <VideoBG />
       <br /><br /><br />
       <TailorDetails />
+      <FacebookMsg />
     </main>
   </div>
   )
