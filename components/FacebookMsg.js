a2215e13a55e850d3275d670b51d39a3da374dc7
NGO HA NAM <ngohanam2709@gmai.com>
2024-08-02 00:20:32 +0700

    facebook1

--- components/FacebookMsg.js
+++ components/FacebookMsg.js
@@ -5,7 +5,7 @@ import { FacebookProvider, CustomChat } from 'react-facebook';
 const FacebookMsg = () => {
     return (
       <FacebookProvider appId="1521376062084598" chatSupport>
-        <CustomChat pageId="411049862084324" minimized={false}/>
+        <CustomChat pageId="61563111945483" minimized={true}/>
       </FacebookProvider>    
     );
 }
