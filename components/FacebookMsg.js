4bca062bb538c55f67e904aec32acf96f3e5f31f
NGO HA NAM <ngohanam2709@gmai.com>
2024-08-02 00:52:20 +0700

    abc

--- components/FacebookMsg.js
+++ components/FacebookMsg.js
@@ -1,13 +1,37 @@
-"use client"
-import React, { Component} from 'react';
-import { FacebookProvider, CustomChat } from 'react-facebook';
+"use clients"
+import React, { useEffect } from 'react';
 
 const FacebookMsg = () => {
-    return (
-      <FacebookProvider appId="1521376062084598" chatSupport>
-        <CustomChat pageId="61563111945483" minimized={true}/>
-      </FacebookProvider>    
-    );
-}
+    useEffect(() => {
+        // Create the script tag and set appropriate attributes
+        const script = document.createElement('script');
+        script.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+        script.async = true;
+        script.defer = true;
+        script.id = 'facebook-jssdk';
+        document.body.appendChild(script);
 
-export default FacebookMsg
+        // Define the window.fbAsyncInit function
+        window.fbAsyncInit = function() {
+            FB.init({
+                xfbml: true,
+                version: 'API-VERSION'  // Replace API-VERSION with the specific version needed
+            });
+        };
+
+        // Return a cleanup function to remove the script when the component unmounts
+        return () => {
+            document.body.removeChild(script);
+        };
+    }, []);  // Empty dependency array ensures this effect only runs once
+
+    return (
+        <div>
+            <div id="fb-root"></div>
+            <div id="fb-customer-chat" className="fb-customerchat"
+                 attribution="biz_inbox" page_id="61563111945483"></div>
+        </div>
+    );
+};
+
+export default FacebookMsg;
